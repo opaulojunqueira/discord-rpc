fdcd6d70fc8325f59f488bdd4ee4ee8b32e40e47
Pauloo <48892662+pauloodev@users.noreply.github.com>
2021-03-24 17:55:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const rpc = require("discord-rpc")
-const client = new rpc.Client({ transport: 'ipc' })
-
-client.on('ready', () => {
-    console.log('\n\n[WARN] This application is a Discord RPC. Any problems please contact the developer.\n\n[!] Application is already running. Do not close this console.')
-    client.request('SET_ACTIVITY', {
-        pid: process.pid,
-        activity: {
-            details: "Your Text Here", // Details of your status here
-            assets: {
-                large_image: "Image", // Image that will be visible to your status
-                large_text: "Your Status" // Image Text. (when you hover over the image)
-            },
-            buttons: [{
-                label: "Button1", // Enter the name for your Button #1
-                url: "Link1" // Insert a url for your Button #1
-            }, {
-                label: "Button2", // Enter the name for your Button #2
-                url: "Link2" // Insert a url for your Button #2
-            }]
-        }
-    })
+const RPC = require('discord-rpc');
+const rpc = new RPC.Client({
+    transport: 'ipc'
 })
 
-client.login({ clientId: "Application ID Here" }).catch(console.error); // Enter the ID of your created Application here
+rpc.on('ready', () => {
+    rpc.setActivity({
+        details: "Your Text Here", // Details of your status here
+        state: "Your Text Here 2", // State of your status here
+        startTimestamp: new Date(), // Shows the elapsed time
+        largeImageKey: "Image", // Image that will be visible to your status
+        largeImageText: "Image Text", // Image Text. (when you hover over the image)
+        buttons: [{
+            label: "Button1", // Enter the name for your Button #1
+            url: "Link1" // Insert a url for your Button #1
+        }]
+        // If you want one more button in your status. Put it like this:
+        // Line 16: }, {
+        // Line 17: label: "Button2",
+        // Line 18: url: "Link2"
+        // Line 19: }]
+    })
+    console.log('\n\n[WARN] This application is a Discord RPC. Any problems please contact the developer.\n\n[!] Application is already running. Do not close this console.')
+});
+
+rpc.login({
+    clientId: "Application ID Here" // Enter the ID of your created Application here
+})
